cb992b6447802621765a81ed0b588ddb0cc57723
Antoine Bichat <a.bichat@yahoo.fr>
2020-02-12 16:30:56 +0100

    Test shifestim class

--- tests/testthat/test-shiftestim.R
+++ tests/testthat/test-shiftestim.R
@@ -22,44 +22,51 @@ estSL <- estimate_shifts(Delta0 = rep(0, N_branch), zscores = zsco_obs,
                          lambda = 1, tree = tree,
                          alpha = 1, method = "scaledlasso")
 estDL <- estimate_shifts(Delta0 = rep(0, N_branch), zscores = zsco_obs,
-                         lambda = 1, tree = tree,
+                         lambda = 1, tree = tree, alpha_conf = 0.01,
                          alpha = 1, method = "desparsifiedlasso")
-
-estimations <- list(estS, estL, estSL)
-estR <- estimations[[sample(x = length(estimations), size = 1)]]
-pR <- plot(estR)
 
 expect_scalnum <- function(x){
   expect_is(x, "numeric")
   expect_equal(length(x), 1)
 }
 
+expect_shiftestim <- function(x){
+  px <- plot(x)
+  expect_equal(class(x), "shiftestim")
+  expect_is(x$zscores_obs, "numeric")
+  expect_is(x$zscores_est, "numeric")
+  expect_is(x$shift_est, "data.frame")
+  expect_is(x$method, "character")
+  expect_is(x$optim_info, "list")
+  expect_is(x$optim_info$supp_arg, "list")
+  expect_is(x$tree, "phylo")
+  expect_is(x$is_bin, "logical")
+  expect_scalnum(x$lambda)
+  expect_scalnum(x$alpha)
+  expect_scalnum(x$sigma)
+  # expect_scalnum(x$objective_value) # Not available for DL for the moment
+  expect_scalnum(x$bic)
+  expect_scalnum(x$pbic)
+  expect_scalnum(x$pars_score)
+  expect_is(px, "ggtree")
+  expect_is(px, "gg")
+  expect_is(px, "ggplot")
+}
+
+
 # General
 
+expect_error(as_shiftestim(tree),
+             "'listopt' must be the output of a optimisation function.")
+
 test_that("Shiftestim class is correct", {
-  expect_error(as_shiftestim(tree),
-               "'listopt' must be the output of a optimisation function.")
-  expect_equal(class(estR), "shiftestim")
-  expect_is(estR$zscores_obs, "numeric")
-  expect_is(estR$zscores_est, "numeric")
-  expect_is(estR$shift_est, "data.frame")
-  expect_is(estR$method, "character")
-  expect_is(estR$optim_info, "list")
-  expect_is(estR$optim_info$supp_arg, "list")
-  expect_is(estR$tree, "phylo")
-  expect_is(estR$is_bin, "logical")
-  expect_scalnum(estR$lambda)
-  expect_scalnum(estR$alpha)
-  expect_scalnum(estR$sigma)
-  # expect_scalnum(estR$objective_value) # Not available for DL for the moment
-  expect_scalnum(estR$bic)
-  expect_scalnum(estR$pbic)
-  expect_scalnum(estR$pars_score)
-  expect_is(pR, "ggtree")
-  expect_is(pR, "gg")
-  expect_is(pR, "ggplot")
+  expect_shiftestim(estS)
+  expect_shiftestim(estL)
+  expect_shiftestim(estSL)
+  expect_shiftestim(estDL)
 })
 
+# Methods specificities
 
 test_that("shooting output is correct", {
   expect_equal(estS$method, "shooting with model selection")
@@ -84,5 +91,16 @@ test_that("shooting output is correct", {
   expect_equal(ncol(estSL$shift_est), 1)
   expect_scalnum(estSL$optim_info$last_progress)
   expect_scalnum(estSL$optim_info$iterations)
+  expect_error(
+    estimate_shifts(Delta0 = rep(0, N_branch), zscores = zsco_obs,
+                    tree = tree, alpha = 1, method = "scaledlasso"),
+    "No model selection can be done with scaledlasso.")
+})
+
+test_that("desparsified output is correct", {
+  expect_equal(estDL$method, "desparsified lasso")
+  expect_equal(ncol(estDL$shift_est), 3)
+  expect_equal(estDL$optim_info$alpha_confint, 0.01)
+  expect_true(is.na(estDL$objective_value))
 })
 
