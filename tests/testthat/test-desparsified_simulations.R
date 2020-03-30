175134ec9469f9b373a043f038d04754c6a92be6
Antoine Bichat <a.bichat@yahoo.fr>
2020-03-30 16:41:06 +0200

    update tests

--- tests/testthat/test-desparsified_simulations.R
+++ tests/testthat/test-desparsified_simulations.R
@@ -32,54 +32,57 @@ test_that("ESD has its specific components / dimensions", {
 })
 
 
-# mat_covar <- covariance_matrix(tree, alpha =  1)
-# mat_incidence <- incidence_matrix(tree)
-# R <- inverse_sqrt(mat_covar)
-# Y <- R %*% zscores
-# X <- R %*% mat_incidence
-# scosys <- calculate_Z(X = X)
-# tau <- noise_factor(X = X, score_system = scosys)
-# V <- covariance_noise_matrix(X = X, score_system = scosys)
-# j <- sample(nplusm, 1)
-# k <- sample(nplusm, 1)
-#
-# scla <- solve_scaled_lasso(y = Y, X = X, beta0 = rep(0, nplusm),
-#                            constraint_type = "beta")
-# beta <- update_beta(X = X, y = Y, beta_init = scla$par$estimate,
-#                     score_system = scosys)
-# hci <- size_half_confint_shifts(noise_factor = tau,
-#                                 hsigma = scla$sigma_scaledlasso)$half_size
-# tau <- noise_factor(X, scosys)
-#
-#
-# test_that("intermediary outputs are correct", {
-#   ## Deterministic
-#   # score system
-#   expect_equal(dim(scosys), dim(X))
-#   # noise factor
-#   expect_length(tau, nplusm)
-#   # covariance noise matrix
-#   expect_equal(V, ESD$optim_info$covariance_noise_matrix)
-#   expect_equal(dim(V), c(nplusm, nplusm))
-#   expect_equal(V, t(V))
-#   expect_equal(V[j, k],
-#                sum(scosys[, j] * scosys[, k]) /
-#                  (abs(sum(scosys[, j] * X[, j]) * sum(scosys[, k] * X[, k]))))
-#   # Noise factor
-#   expect_length(tau, nplusm)
-#   expect_equal(tau, ESD$optim_info$noise_factor)
-#   ## Non-deterministic
-#   # scaled lasso
-#   expect_length(scla, 6)
-#   expect_length(scla$par$estimate, nplusm)
-#   expect_length(scla$sigma_scaledlasso, 1)
-#   expect_named(scla, c("par", "sigma_scaledlasso", "method", "value",
-#                        "iterations", "last_progress"))
-#   # Updated beta
-#   expect_length(beta, nplusm)
-#   # Confidence interval for shifts
-#   expect_length(hci, nplusm)
-# })
+mat_covar <- covariance_matrix(tree, alpha =  1)
+mat_incidence <- incidence_matrix(tree)
+R <- inverse_sqrt(mat_covar)
+Y <- R %*% zscores
+X <- R %*% mat_incidence
+withr::with_preserve_seed({
+  set.seed(42)
+  scosys <- calculate_Z(X = X)
+})
+tau <- noise_factor(X = X, score_system = scosys)
+V <- covariance_noise_matrix(X = X, score_system = scosys)
+j <- sample(nplusm, 1)
+k <- sample(nplusm, 1)
+
+scla <- solve_scaled_lasso(y = Y, X = X, beta0 = rep(0, nplusm),
+                           constraint_type = "beta")
+beta <- update_beta(X = X, y = Y, beta_init = scla$par,
+                    score_system = scosys)
+hci <- size_half_confint_shifts(noise_factor = tau,
+                                hsigma = scla$sigma_scaledlasso)$half_size
+tau <- noise_factor(X, scosys)
+
+
+test_that("intermediary outputs are correct", {
+  ## Deterministic
+  # score system
+  expect_equal(dim(scosys), dim(X))
+  # noise factor
+  expect_length(tau, nplusm)
+  # covariance noise matrix
+  expect_equal(V, ESD$optim_info$covariance_noise_matrix)
+  expect_equal(dim(V), c(nplusm, nplusm))
+  expect_equal(V, t(V))
+  expect_equal(V[j, k],
+               sum(scosys[, j] * scosys[, k]) /
+                 (abs(sum(scosys[, j] * X[, j]) * sum(scosys[, k] * X[, k]))))
+  # Noise factor
+  expect_length(tau, nplusm)
+  expect_equal(tau, ESD$optim_info$noise_factor)
+  ## Non-deterministic
+  # scaled lasso
+  expect_length(scla, 6)
+  expect_length(scla$par, nplusm)
+  expect_length(scla$sigma_scaledlasso, 1)
+  expect_named(scla, c("par", "sigma_scaledlasso", "method", "value",
+                       "iterations", "last_progress"))
+  # Updated beta
+  expect_length(beta, nplusm)
+  # Confidence interval for shifts
+  expect_length(hci, nplusm)
+})
 
 
 test_that("changing confindence interval works", {
