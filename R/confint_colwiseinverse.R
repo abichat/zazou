51fd3de97ef3e7266edb6498ded60ffc5a27c044
Antoine Bichat <a.bichat@yahoo.fr>
2020-04-15 16:13:58 +0200

    bounds

--- R/confint_colwiseinverse.R
+++ R/confint_colwiseinverse.R
@@ -1,34 +1,60 @@
 solve_colwiseinverse <- function(A, gamma){
   dim <- ncol(A)
   M <- matrix(NA, nrow = dim, ncol = dim)
-  for(j in seq_len(dim)){
-    M[, j] <- solve_colwiseinverse_col(A, j, gamma)
+  for(col in seq_len(dim)){
+    M[, col] <- solve_colwiseinverse_col(col, A, gamma)
   }
   return(M)
 }
 
 
-solve_colwiseinverse_col <- function(A, j, gamma){
+solve_colwiseinverse_col <- function(col, A, gamma){
   dim <- ncol(A)
   m <- rep(0, dim)
   for(iter in 1:100){
     sampled_ind <- sample(dim)
     for(ind in sampled_ind){
-      m[ind] <- solve_colwiseinverse_col_cell(A, m, ind, gamma)
+      m[ind] <- solve_colwiseinverse_col_cell(m, ind, col, A, gamma)
     }
   }
-  retrun(m)
+  return(m)
 }
 
-solve_colwiseinverse_col_cell <- function(A, m, ind, gamma){
-  vec_coef_p2 <- coef_p2(A, m, ind)
+solve_colwiseinverse_col_cell <- function(m, ind, col, A, gamma){
+  vec_coef_p2 <- coef_p2(m, ind, A)
+  min_unconstrained <- minimun_p2(vec_coef_p2)
+  bounds <- compute_bounds(m, ind, col, A, gamma)
+
 }
 
-coef_p2 <- function(A, m, ind){
+coef_p2 <- function(m, ind, A){
   a <- A[ind, ind]
   b <- sum(m[-ind] * A[-ind, ind]) + sum(m[-ind] * A[ind, -ind])
-  c <- m[-ind] %*% A[-ind, -ind] %*% m[-ind]
-  return(c(a = a, b = b, c = c))
+  c <- as.vector(m[-ind] %*% A[-ind, -ind] %*% m[-ind])
+  return(list(a = a, b = b, c = c))
+}
+
+minimun_p2 <- function(coef) {
+  if (coef$a > 0) {
+    return(-coef$b / (2 * coef$a))
+  } else if (coef$a == 0) {
+    return(0)
+  } else {
+    return(c(-Inf, Inf))
+  }
+}
+
+compute_bounds <- function(m, ind, col, A, gamma){
+  dim <- length(m)
+  Am_minus_ind <- A[, -ind] %*% m[- ind]
+  # Am_ind <- A[, ind] * m[ind]
+  ecol <- rep(0, dim)
+  ecol[col] <- 1
+  bounds <- matrix(c(-gamma, gamma), byrow = TRUE,
+                   ncol = 2, nrow = dim)
+  bounds <- (bounds + ecol - as.numeric(Am_minus_ind)) / A[, ind]
+  return(list(lower_bound = max(bounds[, 1]),
+              upper_bound = min(bounds[, 2])))
 }
 
 
--- tests/testthat/test-confint_colwiseinverse.R
+++ tests/testthat/test-confint_colwiseinverse.R
@@ -8,17 +8,17 @@ test_that("estimation of polynom coefficients is correct for random 2*2", {
   A <- matrix(a, nrow = 2)
   coef_m1 <- coef_p2(A = A, m = m, ind = 1)
   coef_m2 <- coef_p2(A = A, m = m, ind = 2)
-  expect_equal(coef_m1, c(a = a[1],
-                          b = (a[2] + a[3]) * m[2],
-                          c = a[4] * m[2]^2))
-  expect_equal(coef_m2, c(a = a[4],
-                          b = (a[2] + a[3]) * m[1],
-                          c = a[1] * m[1]^2))
+  expect_equal(coef_m1, list(a = a[1],
+                             b = (a[2] + a[3]) * m[2],
+                             c = a[4] * m[2]^2))
+  expect_equal(coef_m2, list(a = a[4],
+                             b = (a[2] + a[3]) * m[1],
+                             c = a[1] * m[1]^2))
 })
 
 test_that("estimation of polynom coefficients is correct for 3*3", {
   m <- 1:3
   A <- matrix(1:9, ncol = 3, nrow = 3, byrow = TRUE)
   coefs <- coef_p2(A = A, m = m, ind = 3)
-  expect_equal(coefs, c(a = 9, b = 38, c = 33))
+  expect_equal(coefs, list(a = 9, b = 38, c = 33))
 })
