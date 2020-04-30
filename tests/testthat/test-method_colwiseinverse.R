fd16cf693b2e64ff1dfb5e532021b78a874f8755
Antoine Bichat <a.bichat@yahoo.fr>
2020-04-30 11:07:40 +0200

    Fix tests for new arguments

--- tests/testthat/test-method_colwiseinverse.R
+++ tests/testthat/test-method_colwiseinverse.R
@@ -50,31 +50,35 @@ test_that("Computation of m works for easy cases", {
     e[i] <- 1
     ## Small gamma
     expect_identical(
-      fast_solve_colwiseinverse_col(i, A, gamma, e),
+      fast_solve_colwiseinverse_col(col = i, A = A, gamma = gamma, m0 = e),
       matrix((1 - gamma) * e, ncol = 1)
     )
     ## Large gamma
     expect_identical(
-      fast_solve_colwiseinverse_col(i, A, 1, e),
+      fast_solve_colwiseinverse_col(col = i, A = A, gamma = 1, m0 = e),
       matrix(0, nrow = dim, ncol = 1)
     )
   }
 })
 
-test_that("fast_solve_colwiseinverse_col() returns a warning when the initial vector is not feasible.", {
+test_that("fast_solve_colwiseinverse_col() returns a warning/error
+          when the initial vector is not feasible.", {
   dim <- 10
   A <- diag(1, dim)
   gamma <- 0.1
-  expect_warning(fast_solve_colwiseinverse_col(1, A, 0.1, rep(1, dim)),
+  expect_warning(try(fast_solve_colwiseinverse_col(col = 1, A = A, gamma = 0.1, m0 = rep(1, dim)), silent = TRUE),
                  "The starting point is not in the feasible set. Updates may be meaningless.",
                  fixed = TRUE)
+  expect_error(suppressWarnings(fast_solve_colwiseinverse_col(col = 1, A = A, gamma = 0.1, m0 = rep(1, dim))),
+               "No cell could be updated for the first time in column 1.",
+               fixed = TRUE)
 })
 
 test_that("global_constrains are respected", {
   A <- toeplitz(x = 0.5^seq(0, 9))
   # B <- toeplitz(x = c(4/3, -2/3, rep(0, 8))) + diag(rep(c(0, 1/3, 0), times = c(1, 8, 1))) ## = solve(A)
   gamma <- 0.01
-  m <- fast_solve_colwiseinverse_col(1, A, gamma)
+  m <- fast_solve_colwiseinverse_col(col = 1, A = A, gamma = gamma)
   e1 <- c(1, rep(0, 9))
   expect_lte(max(abs(A %*% m - e1)), gamma + sqrt(.Machine$double.eps))
 })
