de6ce7850f3cc1fb1457aa823b435c8718de243c
Antoine Bichat <a.bichat@yahoo.fr>
2020-05-07 17:09:21 +0200

    Clean colwise inverse: use fast version and remove the naive one

--- R/method_colwiseinverse.R
+++ R/method_colwiseinverse.R
@@ -5,7 +5,6 @@
 #' @param ntry_max Integer. Maximum umber of try for each column.
 #' @param silent_on_errors Logical, default to TRUE.
 #' @param silent_on_tries Logical, default to TRUE.
-#' @param fast Method to use.
 #' @param ... Further arguments to be passed to or from other methods.
 #'
 #' @return The column-wise, inverse, same size as \code{A}.
@@ -13,8 +12,7 @@
 #'
 solve_colwiseinverse <- function(A, gamma, ntry_max = 10000,
                                  silent_on_tries = TRUE,
-                                 silent_on_errors = TRUE,
-                                 fast = FALSE, ...){
+                                 silent_on_errors = TRUE, ...){
   ## Validate that A is semi definite positive through SVD decompisition
   if (!isSymmetric(A)) stop("Matrix A should be symmetric.")
   dim <- ncol(A)
@@ -28,13 +26,6 @@ solve_colwiseinverse <- function(A, gamma, ntry_max = 10000,
   gen_inv <- t(U) %*% diag(1 / (d + gamma)) %*% U
   # gen_inv <- solve(A + diag(gamma, nrow = dim))
   M <- matrix(NA, nrow = dim, ncol = dim)
-
-  if(fast){
-    # print("new")
-    solve_col <- fast_solve_colwiseinverse_col
-  } else {
-    solve_col <- solve_colwiseinverse_col
-  }
 
   for(col in seq_len(dim)){
 
@@ -50,7 +41,8 @@ solve_colwiseinverse <- function(A, gamma, ntry_max = 10000,
       # cat("\n####### ntry for col", col, "=", ntry, "#######\n\n")
       col0 <- col0 #+ rnorm(dim, sd = 1 / sqrt(dim))
 
-      new_col <- try(solve_col(col = col, svdA = svd, A = A, gamma = gamma, m0 = col0),
+      new_col <- try(fast_solve_colwiseinverse_col(col = col, svdA = svd, A = A,
+                                                   gamma = gamma, m0 = col0),
                      silent = silent_on_errors)
       ntry <- ntry + 1
     }
@@ -76,193 +68,193 @@ solve_colwiseinverse <- function(A, gamma, ntry_max = 10000,
 }
 
 
-#' Compute a column for the column-wise inverse.
-#'
-#' @param col The column number.
-#' @param m0 Startup column.
-#' @param max_it Maximum number of iterations.
-#' @inheritParams solve_colwiseinverse
-#'
-#' @return The column, wile respecting constrains.
-#' @export
-#'
-solve_colwiseinverse_col <- function(col, A, gamma, m0, max_it = 5000, ...){
-  dim <- ncol(A)
+# #' Compute a column for the column-wise inverse.
+# #'
+# #' @param col The column number.
+# #' @param m0 Startup column.
+# #' @param max_it Maximum number of iterations.
+# #' @inheritParams solve_colwiseinverse
+# #'
+# #' @return The column, wile respecting constrains.
+# #' @export
+# #'
+# solve_colwiseinverse_col <- function(col, A, gamma, m0, max_it = 5000, ...){
+#   dim <- ncol(A)
+#
+#   if(missing(m0)){
+#     # m <- rep(0, dim)
+#     # m <- rnorm(dim, sd = 1 / sqrt(dim))
+#     # m <- rep(1, dim)
+#     m <- sample(x = c(-1, 1), size = dim, replace = TRUE) *
+#                                  rnorm(dim, mean = 1, sd = 1 / sqrt(dim))
+#   } else {
+#     m <- m0
+#   }
+#
+#   # cat("# First m =", m, "\n")
+#
+#
+#   eps <- 10 ^ -8
+#
+#   iter <- 0
+#   obj <- m %*% A %*% m
+#   progress <- +Inf
+#
+#   while(iter < max_it && progress > eps){
+#     sampled_ind <- sample(dim)
+#
+#     for(ind in sampled_ind){
+#       # cat("| > cell:", ind, "\n")
+#       m[ind] <- solve_colwiseinverse_col_cell(m, ind, col, A, gamma)
+#     }
+#
+#     new_obj <- m %*% A %*% m
+#     progress <- abs((obj - new_obj) / obj)
+#     if(is.nan(progress)) progress <- 0
+#     obj <- new_obj
+#     iter <- iter + 1
+#     # cat("##--------------#\n")
+#     # cat("## iter:", iter, "| progress:", progress, "| obj:", obj, "\n")
+#     # cat("## m: ", m, "\n")
+#     # cat("##--------------#\n")
+#   }
+#   # cat("###################\n")
+#   cat("Column ", col, " succeeded after ", iter, " iterations.\n", sep = "")
+#   return(m)
+# }
 
-  if(missing(m0)){
-    # m <- rep(0, dim)
-    # m <- rnorm(dim, sd = 1 / sqrt(dim))
-    # m <- rep(1, dim)
-    m <- sample(x = c(-1, 1), size = dim, replace = TRUE) *
-                                 rnorm(dim, mean = 1, sd = 1 / sqrt(dim))
-  } else {
-    m <- m0
-  }
+# #' Update column cell
+# #'
+# #' @inheritParams solve_colwiseinverse_col
+# #' @param m The column whose a cell need to be updated.
+# #' @param ind The number of the cell in the column.
+# #'
+# #' @return The updated column.
+# #' @export
+# solve_colwiseinverse_col_cell <- function(m, ind, col, A, gamma){
+#   vec_coef_p2 <- coef_p2(m, ind, A)
+#   argmin_unconstrained <- minimun_p2(vec_coef_p2)
+#   # cat("|   >   argmin unconstrained:", argmin_unconstrained, "\n")
+#   bounds <- compute_bounds(m, ind, col, A, gamma)
+#   if(length(argmin_unconstrained) == 1){
+#     argmin <- min(bounds$upper_bound,
+#                   max(bounds$lower_bound, argmin_unconstrained))
+#   } else {
+#     argmin <- argmin_between_two(m, ind, A,
+#                                  bounds$lower_bound,
+#                                  bounds$upper_bound)
+#   }
+#   # cat("|   >   argmin:", argmin, "\n")
+#   if(abs(argmin) < 10^-15) argmin <- 0
+#   return(argmin)
+# }
 
-  # cat("# First m =", m, "\n")
+# #' Compute polynomial coefficients.
+# #'
+# #' Compute coefficients of the polynomial \eqn{m'Am} with the cell
+# #' \code{ind} of \code{m} as the indeterminate.
+# #'
+# #' @inheritParams solve_colwiseinverse_col_cell
+# #'
+# #' @return A list with \code{a}, \code{b} and \code{c} components.
+# #' @export
+# #' @examples
+# #'   m <- c(-1, 4)
+# #'   A <- matrix(c(3, 2, 2, 5), nrow = 2)
+# #'   coef_p2(A = A, m = m, ind = 1)
+# #'   coef_p2(A = A, m = m, ind = 2)
+# coef_p2 <- function(m, ind, A){
+#   a <- A[ind, ind]
+#   b <- sum(m[-ind] * A[-ind, ind]) + sum(m[-ind] * A[ind, -ind, drop = FALSE])
+#   c <- as.vector(m[-ind] %*% A[-ind, -ind, drop = FALSE] %*% m[-ind])
+#   return(list(a = a, b = b, c = c))
+# }
 
+# #' Compute the minimum of a second order polynomial
+# #'
+# #' @param coef A list with \code{a}, \code{b} and \code{c} components.
+# #'
+# #' @return A numeric of length one (eventually \code{Inf} or \code{-Inf})
+# #' if the polynomial if convex. \code{c(-Inf, Inf)} if the polynomial
+# #' is strictly concave.
+# #' @export
+# #' @seealso \code{\link{coef_p2}}
+# #' @examples
+# #' minimun_p2(list(a = 1, b = 2, c = 0))
+# #' minimun_p2(list(a = -1, b = 2, c = 0))
+# #' minimun_p2(list(a = 0, b = 2, c = 0))
+# #' minimun_p2(list(a = 0, b = 0, c = 3))
+# minimun_p2 <- function(coef) {
+#   if (coef$a > 0) {
+#     return(-coef$b / (2 * coef$a))
+#   } else if (coef$a == 0) {
+#     if(coef$b > 0){
+#       return(-Inf)
+#     } else if (coef$b < 0) {
+#       return(Inf)
+#     } else {
+#       return(0)
+#     }
+#   } else {
+#     return(c(-Inf, Inf))
+#   }
+# }
 
-  eps <- 10 ^ -8
+# #' Compute constrains bounds
+# #'
+# #' @inheritParams solve_colwiseinverse_col_cell
+# #'
+# #' @return A list with \code{lower_bound} and \code{upper_bound} components.
+# #' @export
+# #'
+# compute_bounds <- function(m, ind, col, A, gamma){
+#   dim <- length(m)
+#   Am_minus_ind <- A[, -ind, drop = FALSE] %*% m[- ind, drop = FALSE]
+#   # Am_ind <- A[, ind] * m[ind]
+#   ecol <- rep(0, dim)
+#   ecol[col] <- 1
+#   bounds <- matrix(c(-gamma, gamma), byrow = TRUE,
+#                    ncol = 2, nrow = dim)
+#   bounds <- (bounds + ecol - as.numeric(Am_minus_ind)) / A[, ind]
+#   # correct the case when A[, ind] < 0
+#   for(i in seq_len(dim)){
+#     if(A[i, ind] < 0){
+#       bounds[i, ] <- rev(bounds[i, ])
+#     }
+#   }
+#   lb <- max(bounds[, 1])
+#   ub <- min(bounds[, 2])
+#   # cat("|   >   lower bound:", lb, "upper bound:", ub, "\n")
+#   if(lb > ub){
+#     cat("Column ", col, " failed after ", it, " iterations.\n", sep = "")
+#     stop("Constrains are not feasible.")
+#   }
+#   return(list(lower_bound = lb, upper_bound = ub))
+# }
 
-  iter <- 0
-  obj <- m %*% A %*% m
-  progress <- +Inf
-
-  while(iter < max_it && progress > eps){
-    sampled_ind <- sample(dim)
-
-    for(ind in sampled_ind){
-      # cat("| > cell:", ind, "\n")
-      m[ind] <- solve_colwiseinverse_col_cell(m, ind, col, A, gamma)
-    }
-
-    new_obj <- m %*% A %*% m
-    progress <- abs((obj - new_obj) / obj)
-    if(is.nan(progress)) progress <- 0
-    obj <- new_obj
-    iter <- iter + 1
-    # cat("##--------------#\n")
-    # cat("## iter:", iter, "| progress:", progress, "| obj:", obj, "\n")
-    # cat("## m: ", m, "\n")
-    # cat("##--------------#\n")
-  }
-  # cat("###################\n")
-  cat("Column ", col, " succeeded after ", iter, " iterations.\n", sep = "")
-  return(m)
-}
-
-#' Update column cell
-#'
-#' @inheritParams solve_colwiseinverse_col
-#' @param m The column whose a cell need to be updated.
-#' @param ind The number of the cell in the column.
-#'
-#' @return The updated column.
-#' @export
-solve_colwiseinverse_col_cell <- function(m, ind, col, A, gamma){
-  vec_coef_p2 <- coef_p2(m, ind, A)
-  argmin_unconstrained <- minimun_p2(vec_coef_p2)
-  # cat("|   >   argmin unconstrained:", argmin_unconstrained, "\n")
-  bounds <- compute_bounds(m, ind, col, A, gamma)
-  if(length(argmin_unconstrained) == 1){
-    argmin <- min(bounds$upper_bound,
-                  max(bounds$lower_bound, argmin_unconstrained))
-  } else {
-    argmin <- argmin_between_two(m, ind, A,
-                                 bounds$lower_bound,
-                                 bounds$upper_bound)
-  }
-  # cat("|   >   argmin:", argmin, "\n")
-  if(abs(argmin) < 10^-15) argmin <- 0
-  return(argmin)
-}
-
-#' Compute polynomial coefficients.
-#'
-#' Compute coefficients of the polynomial \eqn{m'Am} with the cell
-#' \code{ind} of \code{m} as the indeterminate.
-#'
-#' @inheritParams solve_colwiseinverse_col_cell
-#'
-#' @return A list with \code{a}, \code{b} and \code{c} components.
-#' @export
-#' @examples
-#'   m <- c(-1, 4)
-#'   A <- matrix(c(3, 2, 2, 5), nrow = 2)
-#'   coef_p2(A = A, m = m, ind = 1)
-#'   coef_p2(A = A, m = m, ind = 2)
-coef_p2 <- function(m, ind, A){
-  a <- A[ind, ind]
-  b <- sum(m[-ind] * A[-ind, ind]) + sum(m[-ind] * A[ind, -ind, drop = FALSE])
-  c <- as.vector(m[-ind] %*% A[-ind, -ind, drop = FALSE] %*% m[-ind])
-  return(list(a = a, b = b, c = c))
-}
-
-#' Compute the minimum of a second order polynomial
-#'
-#' @param coef A list with \code{a}, \code{b} and \code{c} components.
-#'
-#' @return A numeric of length one (eventually \code{Inf} or \code{-Inf})
-#' if the polynomial if convex. \code{c(-Inf, Inf)} if the polynomial
-#' is strictly concave.
-#' @export
-#' @seealso \code{\link{coef_p2}}
-#' @examples
-#' minimun_p2(list(a = 1, b = 2, c = 0))
-#' minimun_p2(list(a = -1, b = 2, c = 0))
-#' minimun_p2(list(a = 0, b = 2, c = 0))
-#' minimun_p2(list(a = 0, b = 0, c = 3))
-minimun_p2 <- function(coef) {
-  if (coef$a > 0) {
-    return(-coef$b / (2 * coef$a))
-  } else if (coef$a == 0) {
-    if(coef$b > 0){
-      return(-Inf)
-    } else if (coef$b < 0) {
-      return(Inf)
-    } else {
-      return(0)
-    }
-  } else {
-    return(c(-Inf, Inf))
-  }
-}
-
-#' Compute constrains bounds
-#'
-#' @inheritParams solve_colwiseinverse_col_cell
-#'
-#' @return A list with \code{lower_bound} and \code{upper_bound} components.
-#' @export
-#'
-compute_bounds <- function(m, ind, col, A, gamma){
-  dim <- length(m)
-  Am_minus_ind <- A[, -ind, drop = FALSE] %*% m[- ind, drop = FALSE]
-  # Am_ind <- A[, ind] * m[ind]
-  ecol <- rep(0, dim)
-  ecol[col] <- 1
-  bounds <- matrix(c(-gamma, gamma), byrow = TRUE,
-                   ncol = 2, nrow = dim)
-  bounds <- (bounds + ecol - as.numeric(Am_minus_ind)) / A[, ind]
-  # correct the case when A[, ind] < 0
-  for(i in seq_len(dim)){
-    if(A[i, ind] < 0){
-      bounds[i, ] <- rev(bounds[i, ])
-    }
-  }
-  lb <- max(bounds[, 1])
-  ub <- min(bounds[, 2])
-  # cat("|   >   lower bound:", lb, "upper bound:", ub, "\n")
-  if(lb > ub){
-    cat("Column ", col, " failed after ", it, " iterations.\n", sep = "")
-    stop("Constrains are not feasible.")
-  }
-  return(list(lower_bound = lb, upper_bound = ub))
-}
-
-#' Find the argmin between two propositions
-#'
-#' @details The function to minimize is \eqn{m'Am}.
-#'
-#' @inheritParams solve_colwiseinverse_col_cell
-#' @param prop1 First proposition for the \code{ind} cell in \code{m}.
-#' @param prop2 Second proposition for the \code{ind} cell in \code{m}
-#'
-#' @return \code{prop1} or \code{prop2}.
-#' @export
-#'
-argmin_between_two <- function(m, ind, A, prop1, prop2){
-  m_1 <- m_2 <- m
-  m_1[ind] <- prop1
-  m_2[ind] <- prop2
-  mAm_1 <- m_1 %*% A %*% m_1
-  mAm_2 <- m_2 %*% A %*% m_2
-  if(mAm_1 < mAm_2) {
-    return(prop1)
-  } else {
-    return(prop2)
-  }
-}
+# #' Find the argmin between two propositions
+# #'
+# #' @details The function to minimize is \eqn{m'Am}.
+# #'
+# #' @inheritParams solve_colwiseinverse_col_cell
+# #' @param prop1 First proposition for the \code{ind} cell in \code{m}.
+# #' @param prop2 Second proposition for the \code{ind} cell in \code{m}
+# #'
+# #' @return \code{prop1} or \code{prop2}.
+# #' @export
+# #'
+# argmin_between_two <- function(m, ind, A, prop1, prop2){
+#   m_1 <- m_2 <- m
+#   m_1[ind] <- prop1
+#   m_2[ind] <- prop2
+#   mAm_1 <- m_1 %*% A %*% m_1
+#   mAm_2 <- m_2 %*% A %*% m_2
+#   if(mAm_1 < mAm_2) {
+#     return(prop1)
+#   } else {
+#     return(prop2)
+#   }
+# }
 
 
 
@@ -411,7 +403,7 @@ fast_solve_colwiseinverse_col <- function(col, svdA, A, gamma, m0, max_it = 5000
 
   ## Checkfor problems
   if (it == max_it) warning("Convergence not reached for column ", col, ".")
-  cat("Column ", col, " succeeded after ", it, " iterations.\n", sep = "")
+  # cat("Column ", col, " succeeded after ", it, " iterations.\n", sep = "")
   ## return solution
   U %*% m
 }
@@ -440,7 +432,7 @@ update_cell <- function(c, b, gamma) {
   lower_bound <- max(ifelse(bound_1 > bound_2, bound_2, bound_1))
   ## Check that feasible set is not empty
   if (upper_bound - lower_bound < sqrt(.Machine$double.eps)) {
-    cat("Column ", col, " failed after ", it, " iterations.\n", sep = "")
+    # cat("Column ", col, " failed after ", it, " iterations.\n", sep = "")
     stop("Feasible set is empty.")
   }
   ## Project 0 on feasibility set and return result
--- tests/testthat/test-method_colwiseinverse.R
+++ tests/testthat/test-method_colwiseinverse.R
@@ -1,33 +1,33 @@
 context("Column-wise inverse matrix")
 
 
-test_that("estimation of polynom coefficients is correct for random 2*2", {
-  m <- rnorm(2)
-  a <- rnorm(4)
-  A <- matrix(a, nrow = 2)
-  coef_m1 <- coef_p2(A = A, m = m, ind = 1)
-  coef_m2 <- coef_p2(A = A, m = m, ind = 2)
-  expect_equal(coef_m1, list(a = a[1],
-                             b = (a[2] + a[3]) * m[2],
-                             c = a[4] * m[2]^2))
-  expect_equal(coef_m2, list(a = a[4],
-                             b = (a[2] + a[3]) * m[1],
-                             c = a[1] * m[1]^2))
-})
+# test_that("estimation of polynom coefficients is correct for random 2*2", {
+#   m <- rnorm(2)
+#   a <- rnorm(4)
+#   A <- matrix(a, nrow = 2)
+#   coef_m1 <- coef_p2(A = A, m = m, ind = 1)
+#   coef_m2 <- coef_p2(A = A, m = m, ind = 2)
+#   expect_equal(coef_m1, list(a = a[1],
+#                              b = (a[2] + a[3]) * m[2],
+#                              c = a[4] * m[2]^2))
+#   expect_equal(coef_m2, list(a = a[4],
+#                              b = (a[2] + a[3]) * m[1],
+#                              c = a[1] * m[1]^2))
+# })
 
-test_that("estimation of polynom coefficients is correct for 3*3", {
-  m <- 1:3
-  A <- matrix(1:9, ncol = 3, nrow = 3, byrow = TRUE)
-  coefs <- coef_p2(A = A, m = m, ind = 3)
-  expect_equal(coefs, list(a = 9, b = 38, c = 33))
-})
+# test_that("estimation of polynom coefficients is correct for 3*3", {
+#   m <- 1:3
+#   A <- matrix(1:9, ncol = 3, nrow = 3, byrow = TRUE)
+#   coefs <- coef_p2(A = A, m = m, ind = 3)
+#   expect_equal(coefs, list(a = 9, b = 38, c = 33))
+# })
 
 dim <- 10
 gamma <- 2 * sqrt(log(dim)/dim)
 # gamma <- 1
 X <- matrix(rnorm(dim^2), ncol = dim) / dim
 A <- t(X) %*% X
-M <- try(solve_colwiseinverse(A, gamma))
+M <- suppressWarnings(try(solve_colwiseinverse(A, gamma)))
 
 while(!is.matrix(M)){
   M <- try(solve_colwiseinverse(A, gamma))
